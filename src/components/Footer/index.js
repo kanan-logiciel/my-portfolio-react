c53d1ede26afab74c12da363e5bb26b4da2d43d8
Kanan048 <159876773+Kanan048@users.noreply.github.com>
2024-03-18 16:14:42 +0530

    chnages created

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,55 +1,76 @@
-import React from 'react';
+import React from "react";
 
 import "./index.css";
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <footer className="footer mt-auto py-3 main-content">
       <Container>
-        <Row>
+        <Row className="footer-content">
           <Col md={3}>
             <h5>About</h5>
             <ul className="list-parts">
-              <li><a href="1">About Us</a></li>
-              <li><a href="2">Our Team</a></li>
-              <li><a href="2">Testimonials</a></li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Our Team</a>
+              </li>
+              <li>
+                <a href="#">Testimonials</a>
+              </li>
             </ul>
           </Col>
           <Col md={3}>
             <h5>Facts</h5>
             <ul className="list-parts">
-              <li><a href="1">Company Facts</a></li>
-              <li><a href="1">Statistics</a></li>
+              <li>
+                <a href="#">Company Facts</a>
+              </li>
+              <li>
+                <a href="#">Statistics</a>
+              </li>
             </ul>
           </Col>
           <Col md={3}>
             <h5>Portfolio</h5>
             <ul className="list-parts">
-              <li><a href="1">Projects</a></li>
-              <li><a href="1">Client Work</a></li>
+              <li>
+                <a href="#">Projects</a>
+              </li>
+              <li>
+                <a href="#">Client Work</a>
+              </li>
             </ul>
           </Col>
           <Col md={3}>
             <h5>Resume</h5>
             <ul className="list-parts">
-              <li><a href="1">Experience</a></li>
-              <li><a href="1">Education</a></li>
-              <li><a href="1">Skills</a></li>
+              <li>
+                <a href="#">Experience</a>
+              </li>
+              <li>
+                <a href="#">Education</a>
+              </li>
+              <li>
+                <a href="#">Skills</a>
+              </li>
             </ul>
           </Col>
         </Row>
         <hr />
         <Row>
           <Col>
-            <p className="text-muted text-center">myPortfolio&copy; {new Date().getFullYear()}</p>
+            <p className="text-muted text-center">
+              myPortfolio&copy; {new Date().getFullYear()}
+            </p>
           </Col>
         </Row>
       </Container>
     </footer>
   );
-}
+};
 
 export default Footer;
-
